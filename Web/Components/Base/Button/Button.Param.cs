5b6e88de05ebcf51a85d5768b076e32e0c3300f0
NFiveDev <nmhjort7@gmail.com>
2025-06-24 21:41:33 +0200

    Scaffolding Button.razor

---
+++ Web/Components/Base/Button/Button.Param.cs
@@ -0,0 +1,16 @@
+namespace Web.Components.Base.Button;
+
+public enum ButtonVariant
+{
+    Primary,
+    Secondary,
+    Outline,
+    Icon
+}
+
+public enum ButtonSize
+{
+    Small,
+    Medium,
+    Large,
+}
